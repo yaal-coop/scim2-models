3e9f7fc182a8328975f8eb9bd7ff9d537f34de7a
Éloi Rivard <eloi@yaal.coop>
2024-08-30 14:06:30 +0200

    doc: hide some autodoc_pydantic useless data

--- doc/conf.py
+++ doc/conf.py
@@ -75,6 +75,10 @@ html_context = {
 autodoc_pydantic_model_show_config_summary = False
 autodoc_pydantic_model_show_field_summary = False
 autodoc_pydantic_model_show_json = False
+autodoc_pydantic_model_show_validator_summary = False
+autodoc_pydantic_model_show_validator_members = False
+autodoc_pydantic_field_show_constraints = False
+autodoc_pydantic_field_list_validators = False
 autodoc_pydantic_field_doc_policy = "docstring"
 
 # -- Options for doctest -------------------------------------------
